bcedd5e6ad2ed58e35a2839e12481eb398cf48e9
Endah Fitria N <76398931+Fiindah@users.noreply.github.com>
2022-11-09 16:30:15 +0700

    Add files via upload

---
+++ src/script/data/foods.js
@@ -0,0 +1,32 @@
+const foods = [
+  {
+    name: 'Pancakes',
+    ingredients: '100g Flour, 2 large Eggs, 300ml Milk, 1 tbls Sunflower Oil, to serve Sugar, to serve Raspberries, to serve Blueberries.',
+    fanArt: 'https://www.themealdb.com/images/media/meals/rwuyqx1511383174.jpg',
+    instructions: 'Put the flour, eggs, milk, 1 tbsp oil and a pinch of salt into a bowl or large jug, then whisk to a smooth batter. Set aside for 30 mins to rest if you have time, or start cooking straight away. Set a medium frying pan or crêpe pan over a medium heat and carefully wipe it with some oiled kitchen paper. When hot, cook your pancakes for 1 min on each side until golden, keeping them warm in a low oven as you go. Serve with lemon wedges and sugar, or your favourite filling. Once cold, you can layer the pancakes between baking parchment, then wrap in cling film and freeze for up to 2 months.',
+  },
+  {
+    name: 'Lasagna Sandwiches',
+    ingredients: '1/4 cup Sour Cream, 2tbs Chopped Onion, 1/2 tbs Dried Oregano, 1/2 tsp Salt, 8 slices Bread, 8 slices Bacon, 8 slices tomato, 4 slices Mozzarella, 2 1/2 tbs Butter.',
+    fanArt: 'https://www.themealdb.com/images/media/meals/xr0n4r1576788363.jpg',
+    instructions: 'In a small bowl, combine the first four ingredients; spread on four slices of bread. Layer with bacon, tomato and cheese; top with remaining bread. 2. In a large skillet or griddle, melt 2 tablespoons butter. Toast sandwiches until lightly browned on both sides and cheese is melted, adding butter if necessary. Nutrition Facts 1 sandwich: 445 calories, 24g fat (12g saturated fat), 66mg cholesterol, 1094mg sodium, 35g carbohydrate (3g sugars, 2g fiber), 21g protein.',
+  },
+  {
+    name: 'Krispy Kreme Donut',
+    ingredients: '1/4 ounce Yeast, 1 1/2 cup Water, 1/2 cup Sugar. 1 tsp Salt, 2 Eggs, 1/3 cup Shortening, 5 drops Flour, Sprinking Canola Oil, 1/2 cup Milk, 2 cups Sugar, 1 1/2 cups Vanilla, 6 tbs Boiling Water, 1/3 cup Butter.',
+    fanArt: 'https://www.themealdb.com/images/media/meals/4i5cnx1587672171.jpg',
+    instructions: 'Dissolve yeast in warm water in 2 1/2-quart bowl. Add milk, sugar, salt, eggs, shortening and 2 cups flour. Beat on low for 30 seconds, scraping bowl constantly. Beat on medium speed for 2 minutes, scraping bowl occasionally. Stir in remaining flour until smooth. Cover and let rise until double, 50-60 minutes. (Dough is ready when indentation remains when touched.) Turn dough onto floured surface; roll around lightly to coat with flour. Gently roll dough 1/2-inch thick with floured rolling pin. Cut with floured doughnut cutter. Cover and let rise until double, 30-40 minutes. Heat vegetable oil in deep fryer to 350°. Slide doughnuts into hot oil with wide spatula. Turn doughnuts as they rise to the surface. Fry until golden brown, about 1 minute on each side. Remove carefully from oil (do not prick surface); drain. Dip the doughnuts into creamy glaze set on rack. Glaze: Heat butter until melted. Remove from heat. Stir in powdered sugar and vanilla until smooth. Stir in water, 1 tablespoon at a time, until desired consistency.',
+  },
+  {
+    nama: 'Salted Caramel Cheescake',
+    ingredients: '250g Digestive Biscuits, 75g Pretzels, 135g Butter, 450g Cream Cheese, 1tsp Vanilla Extract, 100g Icing Sugar, 150g caramel, 1tsp Sea Salt, 300ml Double Cream, drizzle Caramel Sauce, Top Toffee Popcorn, Top Pretzels.',
+    fanArt: 'https://www.themealdb.com/images/media/meals/xqrwyr1511133646.jpg',
+    instructions: ' Blitz the biscuits and the pretzels in a food processor and mix the biscuits with the melted butter. Spread on the bottom of an 8″/20cm Deep Springform Tin and press down firmly. Leave to set in the fridge whilst you make the rest! 2) Using an electric mixer, I use my KitchenAid with the whisk attachment, whisk together the cream cheese, vanilla, and icing sugar until smooth and then add the caramel and whisk again until smooth and lump free – this could take a couple of minutes, I whisk it at half speed so not too quick or slow! 3) Pour in the double cream & Salt flakes and continue to whisk for a couple of minutes until its very thick and mousse like (I mix it on a medium speed, level 6/10) – Now this could take up to 5 minutes depending on your mixer, but you seriously have to stick at it – it will hold itself completely when finished mixing (like a meringue does!) If you don’t mix it enough it will not set well enough, but don’t get impatient and whisk it really quick because that’ll make it split! Spread over the biscuit base and leave to set in the fridge overnight. Remove the Cheesecake from the tin carefully and decorate the cheesecake – I drizzled over some of the spare caramel, and then some Toffee Popcorn and more Pretzels!.',
+  },
+  {
+    name: 'Tunisian Orange Cake',
+    ingredients: '1 large Orange, 300g Caster Sugar, 75ml Olive Oil, 280g Flour, 1 tbs Baking Powder, 4 large Eggs, 2 tsp Vanilla Extract.',
+    fanArt: 'https://www.themealdb.com/images/media/meals/y4jpgq1560459207.jpg',
+    instructions: 'Preheat oven to 190 C / Gas 5. Grease a 23cm round springform tin. Cut off the hard bits from the top and bottom of the orange. Slice the orange and remove all seeds. Puree the orange with its peel in a food processor. Add one third of the sugar and the olive oil and continue to mix until well combined. Sieve together flour and baking powder. Beat the eggs and the remaining sugar with an electric hand mixer for at least five minutes until very fluffy. Fold in half of the flour mixture, then the orange and the vanilla, then fold in the remaining flour. Mix well but not for too long. Pour cake mixture into prepared tin and smooth out. Bake in preheated oven for 20 minutes. Reduce the oven temperature to 160 C / Gas 2 and bake again for 30 minutes Bake until the cake is golden brown and a skewer comes out clean. Cool on a wire cake rack.',
+  }
+];
